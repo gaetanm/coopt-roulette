69a8a25a44d02e8f439900a018ec4f77fb4282d7
Gaëtan Masson <im.gaetanmasson@gmail.com>
2015-07-21 17:52:02 +0200

    Add the random method

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -4,6 +4,10 @@ class JobOffer
   attr_accessor :id, :title, :description, :salary, :internal_reference,
   :company_name, :status, :published_at, :closed_at, :created_at, :updated_at
   
+  def initialize(attributes={})
+    super
+  end
+  
   def self.find(id)
     get_json_data.each do |v|
       return self.new v if v['id'] == id
@@ -15,13 +19,14 @@ class JobOffer
     get_json_data.length
   end
 
-  def initialize(attributes={})
-    super
+  def self.random
+    r = Random.new
+    self.find r.rand(self.count)
   end
 
-  private
   def self.get_json_data
     data = File.read(Rails.root + 'test/fixtures/job_offers.json')
     JSON.parse(data)
   end
+  private_class_method :get_json_data
 end
