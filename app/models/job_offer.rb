4897b45e66394ec37f902c8d870b1f9fe7a3e0bd
Gaëtan Masson <im.gaetanmasson@gmail.com>
2015-07-21 17:35:23 +0200

    Add count and find method to the model

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,12 +1,27 @@
 class JobOffer
-  include ActiveModel::Serializers::JSON
+  include ActiveModel::Model
 
-  attr_accessor :title, :description, :salary, :internal_reference,
+  attr_accessor :id, :title, :description, :salary, :internal_reference,
   :company_name, :status, :published_at, :closed_at, :created_at, :updated_at
-
-  def attributes=(hash)
-    hash.each do |key, value|
-      send("#{key}=", value)
+  
+  def self.find(id)
+    get_json_data.each do |v|
+      return self.new v if v['id'] == id
     end
+    return nil
+  end
+
+  def self.count
+    get_json_data.length
+  end
+
+  def initialize(attributes={})
+    super
+  end
+
+  private
+  def self.get_json_data
+    data = File.read(Rails.root + 'test/fixtures/job_offers.json')
+    JSON.parse(data)
   end
 end
