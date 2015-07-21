fddbe8d48028607b421664153631879a7fa7bfdd
Gaëtan Masson <im.gaetanmasson@gmail.com>
2015-07-21 13:31:49 +0200

    Fix bad method name

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,7 +1,7 @@
 class JobOffer
   include ActiveModel::Serializers::JSON
 
-  attr_acccessor :title, :description, :salary, :internal_reference,
+  attr_accessor :title, :description, :salary, :internal_reference,
   :company_name, :status, :published_at, :closed_at, :created_at, :updated_at
 
   def attributes=(hash)
