b4b19c1f2a03a33652357768dde59e7a94437229
Gaëtan Masson <im.gaetanmasson@gmail.com>
2015-07-22 18:02:35 +0200

    Add the create method to Recommendations

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,18 @@
 class RecommendationsController < ApplicationController
+  def create
+    @recommendation = Recommendation.new recommendation_params
+    if @recommendation.valid?
+      flash[:info] = 'Recommendation sucessfuly sent.'
+      redirect_to root_path
+    else
+      @job_offer = JobOffer.find(recommendation_params[:job_offer_id].to_i)
+      render 'job_offers/random'
+    end
+  end
+
+  private
+    def recommendation_params
+      params.require(:recommendation).permit(:male, :email, :first_name, :last_name, 
+        :phone, :company, :job_title, :linkedin_url, :job_offer_id)
+    end
 end
