d60e2fb630574ec360c4432cc29c333f8f645159
Gaëtan Masson <im.gaetanmasson@gmail.com>
2015-07-22 17:19:11 +0200

    Fill in the Recommendation model

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,2 +1,18 @@
-class Recommendation < ActiveRecord::Base
+class Recommendation
+  include ActiveModel::Model
+  include ActiveModel::Validations
+
+  attr_accessor :male, :first_name, :last_name, :email, :phone,
+  :company, :job_title, :linkedin_url, :job_offer_id, :created_at, :updated_at
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :phone, presence: true, unless: ->(recommendation){recommendation.email.present? or recommendation.linkedin_url.present?}
+  validates :email, presence: true, unless: ->(recommendation){recommendation.phone.present? or recommendation.linkedin_url.present?}
+  validates :linkedin_url, presence: true, unless: ->(recommendation){recommendation.email.present? or recommendation.phone.present?}
+
+  def initialize(attributes={})
+    super
+  end
 end
