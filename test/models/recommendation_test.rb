41a8dc0b15c3044dc1040836f5a7166281cdacc3
Gaëtan Masson <im.gaetanmasson@gmail.com>
2015-07-25 11:02:41 +0200

    Add tests for Recommendation

--- test/models/recommendation_test.rb
+++ test/models/recommendation_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class RecommendationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @recommendation = Recommendation.new phone: "", email: "", linkedin_url: ""
+  end
+
+  test "first name should be present" do
+    @recommendation.first_name = ""
+    assert_not @recommendation.valid?
+  end
+
+  test "last name should be present" do
+    @recommendation.last_name = ""
+    assert_not @recommendation.valid?
+  end
+
+  test "email xor phone xor linkedin url should be present" do
+    @recommendation.first_name = "Foo"
+    @recommendation.last_name = "Bar"
+    assert_not @recommendation.valid?
+  end
+
+  test "email xor phone xor linkedin url is present" do
+    @recommendation.email = "foo@tbar.com"
+    @recommendation.first_name = "Foo"
+    @recommendation.last_name = "Bar"
+    assert @recommendation.valid?
+  end
 end
